1b47423d40f7a7563029c78bf4fa465975fac092
marshapp <malofreix@gmail.com>
2025-01-06 18:32:12 +0100

    ara si

---
+++ 3)/energia.f90
@@ -0,0 +1,48 @@
+program energia
+
+    implicit none(type,external)
+    integer, parameter :: dp = selected_real_kind(15, 300), punts_per_dia = 1440, n = 365 * punts_per_dia, num_cols = 11
+    integer :: i, dia, j
+    real(kind=dp) :: potencies(n, num_cols), energies(365, num_cols), h, integral, dummy
+
+    !Llegim les dades del fitxer "any_pot_dades.txt" ignorant la primera columna i les afegim a la matriu potencies(,)
+    open(unit=10, file='any_pot_dades.txt', status='old', action='read')
+    do i = 1, n
+        read(10, *) dummy, (potencies(i, j), j = 1, num_cols)
+    end do
+    close(10)
+  
+    !Definim l'interval temporal de les dades dividint els segons que hi ha
+    !en un dia entre el nombre d'intervals que hi ha en un dia
+    h = 86400_dp / real(punts_per_dia - 1)
+  
+    !Claculem numèricament l'energia utilitzant Simpson 1/3 per diferents combinacions d'angles beta i gamma
+    !i guardem els resultats a la matriu energies(,)
+    do j = 1, num_cols
+      do dia = 1, 365
+        integral = 0.0_dp
+  
+        integral = potencies((dia - 1) * punts_per_dia + 1, j) + potencies(dia * punts_per_dia, j)
+
+        do i = (dia - 1) * punts_per_dia + 2, dia * punts_per_dia - 1, 2
+          integral = integral + 4.0_dp * potencies(i, j)
+        end do
+
+        do i = (dia - 1) * punts_per_dia + 3, dia * punts_per_dia - 2, 2
+          integral = integral + 2.0_dp * potencies(i, j)
+        end do
+
+        integral = integral * (h / 3.0_dp)
+  
+        energies(dia, j) = integral
+      end do
+    end do
+  
+    !Guardem els resultats al fitxer "energies.txt"
+    open(unit=20, file='energies.txt', status='replace', action='write')
+    do dia = 1, 365
+      write(20, *) dia, (energies(dia, j), j = 1, num_cols)
+    end do
+    close(20)
+  
+  end program energia
---
+++ extra_optimitzacio_angles/optimitzacio.f90
@@ -0,0 +1,52 @@
+program opt
+
+    implicit none(type, external)
+    integer, parameter :: dp = selected_real_kind(15, 300), punts_per_dia = 1440, n = 365 * punts_per_dia
+    real(kind=dp), dimension(n) :: integrand_a, integrand_b, integrand_c
+    real(kind=dp) :: a, b, c, h, gamma, beta, pi = 2.0*acos(0.0)
+    integer :: i
+  
+    !Obrim l'arxiu que conté els 3 integrands a cada minut de l'any
+    open(unit=10, file='integrands.txt', status='old', action='read')
+    do i = 1, n
+      read(10, *) integrand_a(i), integrand_b(i), integrand_c(i)
+    end do
+    close(10)
+
+    !Definim l'interval temporal de les dades dividint els segons que hi ha
+    !en un dia entre el nombre d'intervals que hi ha en un dia
+    h = 86400.0_dp / real(punts_per_dia-1, kind=dp)
+  
+    !Claculem numèricament les integrals a, b i c utilitzant Simpson 1/3 
+    a = integrand_a(1) + integrand_a(n)
+    b = integrand_b(1) + integrand_b(n)
+    c = integrand_c(1) + integrand_c(n)
+  
+    do i = 2, n - 1, 2
+      a = a + 4.0_dp * integrand_a(i)
+      b = b + 4.0_dp * integrand_b(i)
+      c = c + 4.0_dp * integrand_c(i)
+    end do
+  
+    do i = 3, n - 2, 2
+      a = a + 2.0_dp * integrand_a(i)
+      b = b + 2.0_dp * integrand_b(i)
+      c = c + 2.0_dp * integrand_c(i)
+    end do
+  
+    a = a * h / 3.0_dp
+    b = b * h / 3.0_dp
+    c = c * h / 3.0_dp
+  
+    !Calculem l'angle gamma
+    gamma = atan(c/b)
+  
+    !Calculem l'angle beta
+    beta = atan((b*cos(gamma)+c*sin(gamma))/a)
+  
+    !Imprimim a la terminal els resultats amb un decimal i en graus
+    write(*,'(A,F6.1,A)') "L'angle gamma òptim és:", gamma*180.0_dp/pi, "°"
+    write(*,'(A,F6.1,A)') "L'angle beta òptim és:", beta*180.0_dp/pi, "°"
+  
+  end program opt
+  
